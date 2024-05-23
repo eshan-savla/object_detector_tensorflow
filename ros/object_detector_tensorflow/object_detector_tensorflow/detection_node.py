430f8c05b66caae3746dd8e17249a4371583ceb1
Eshan <eshan.savla@gmail.com>
2024-05-23 14:05:02 +0200

    added multithreading for parallel service and subscription

--- ros/object_detector_tensorflow/object_detector_tensorflow/detection_node.py
+++ ros/object_detector_tensorflow/object_detector_tensorflow/detection_node.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
 
 from time import sleep
+import threading
 import rclpy
+import rclpy.executors
 import rclpy.time
 from sensor_msgs.msg import Image
 
+from rclpy.executors import MultiThreadedExecutor
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from object_detector_tensorflow.base_node import ObjectDetectionBaseNode
 from object_detector_tensorflow_interfaces.msg import Detections
 from object_detector_tensorflow_interfaces.srv import DetectObjects
@@ -17,12 +21,16 @@ class DetectionNode(ObjectDetectionBaseNode):
 
         super().__init__(node_name)
 
+        self.service_callback_group = MutuallyExclusiveCallbackGroup()
         self.service = self.create_service(
-            DetectObjects, f"{node_name}/detect_objects", self._detect_objects)
+            DetectObjects, f"{node_name}/detect_objects", self._detect_objects, callback_group=self.service_callback_group)
         
+        self.image_lock = threading.Lock()
         self.latest_img: Image = None
 
-        self.image_subscriber = self.create_subscription(Image, self.image_topic, self._subscriber_callback, 1)
+        self.subscriber_callback_group = MutuallyExclusiveCallbackGroup()
+        
+        self.image_subscriber = self.create_subscription(Image, self.image_topic, self._subscriber_callback, 1, callback_group=self.subscriber_callback_group)
 
         self.image_publisher = self.create_publisher(
             Image, f"{node_name}/result_image", 1)
@@ -31,14 +39,17 @@ class DetectionNode(ObjectDetectionBaseNode):
             Detections, f"{node_name}/detections", 1)
 
     def _subscriber_callback(self, msg: Image) -> None:
-        self.latest_img = msg
+        if self.latest_img is None:
+            self.get_logger().info("Received image.")
+        with self.image_lock:
+            self.latest_img = msg
 
     def _detect_objects(self,
                         request: DetectObjects.Request,
                     response: DetectObjects.Response) -> DetectObjects.Response:
+        self.get_logger().info("Received request to detect objects.")
         while self.latest_img is None:
-            self.get_logger().info("Waiting for image...", throttle_duration_sec=2.0)
-            sleep(0.1)
+            self.get_logger().info("Waiting for image...", throttle_duration_sec=2.0)    
         response.reference_image = self.latest_img
         detected_objects, result_image = super()._detect_objects(
             self.latest_img)
@@ -48,7 +59,10 @@ class DetectionNode(ObjectDetectionBaseNode):
         self.image_publisher.publish(result_image)
 
         self.detections_publisher.publish(detected_objects)
-        self.latest_img = None
+        self.get_logger().info("Published detections and result image.")
+        with self.image_lock:
+            self.latest_img = None
+        self.get_logger().info("Reset latest image.")
         return response
 
 
@@ -56,7 +70,25 @@ def main(args=None) -> None:
 
     rclpy.init(args=args)
 
-    DetectionNode().run()
+    # DetectionNode().run()
+
+    # rclpy.init(args=args)
+    executor = MultiThreadedExecutor(num_threads=3)
+
+    node = DetectionNode()
+    executor.add_node(node)
+
+    try:
+        node.logger.info("Started Node")
+
+        executor.spin()
+
+    except KeyboardInterrupt:
+        node.logger.info("Stopped Node")
+
+    executor.shutdown()
+    node.destroy_node()
+
 
 
 if __name__ == '__main__':
