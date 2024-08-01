50ade3e0f42d5823691cce6ef8db12ecd1d289d2
Eshan Savla <eshan.savla@gmail.com>
2024-08-01 14:24:06 +0000

    added option to start without gpu

--- start_docker.sh
+++ start_docker.sh
@@ -2,18 +2,33 @@
 
 xhost + local:root
 
-docker run \
-    -it \
-    --privileged \
-    --net host \
-    -e DISPLAY=$DISPLAY \
-    --env-file .env \
-    --rm \
-    --name odtf \
-    --gpus all \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
-    -v $PWD/:/home/docker/ros2_ws/src/object_detector_tensorflow/ \
-    -v $PWD/.vscode:/home/docker/ros2_ws/src/.vscode \
-    object_detector_tensorflow/ros:humble
+if [ "$1" = "--no-gpus" ]; then
+    docker run \
+        -it \
+        --privileged \
+        --net host \
+        -e DISPLAY=$DISPLAY \
+        --env-file .env \
+        --rm \
+        --name odtf \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v $PWD/:/home/docker/ros2_ws/src/object_detector_tensorflow/ \
+        -v $PWD/.vscode:/home/docker/ros2_ws/src/.vscode \
+        object_detector_tensorflow/ros:humble
+else
+    docker run \
+        -it \
+        --privileged \
+        --net host \
+        -e DISPLAY=$DISPLAY \
+        --env-file .env \
+        --rm \
+        --name odtf \   
+        --gpus all \
+        -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -v $PWD/:/home/docker/ros2_ws/src/object_detector_tensorflow/ \
+        -v $PWD/.vscode:/home/docker/ros2_ws/src/.vscode \
+        object_detector_tensorflow/ros:humble
+fi
     # -v $PWD/ros/object_detector_tensorflow/data:/home/docker/ros2_ws/src/object_detector_tensorflow/ros/object_detector_tensorflow/data \
     
